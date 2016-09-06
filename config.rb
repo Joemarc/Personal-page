b9f344f28c3151baddd2edc48718cb29eb33a530
Joe Marc <joe.marc3@gmail.com>
2016-09-06 19:00:17 +0200

    added some html and css

--- config.rb
+++ config.rb
@@ -86,3 +86,7 @@ activate :deploy do |deploy|
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
+# ["run2meet", "coursmapoule"].each do |name|
+#   proxy "/projects/#{name}.html", "/projects/show.html", :locals => { :owner => name }, :ignore => true
+# end
